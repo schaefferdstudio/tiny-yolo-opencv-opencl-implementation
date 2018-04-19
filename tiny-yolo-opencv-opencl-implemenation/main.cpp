879292f9cf93097547410587982b37754104a0b7
Daniel <9c23@oth-aw.de>
2018-04-19 10:09:18 +0200

    Initial Commit

---
+++ tiny-yolo-opencv-opencl-implemenation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tiny-yolo-opencv-opencl-implemenation
+//
+//  Created by Daniel Sch on 19.04.18.
+//  Copyright © 2018 Daniel Sch. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
